b3e4d72ba8b2df99a43c020457ca1482eb2bbf59
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2024-06-24 22:45:39 -0300

    ✨ feat(Event): Substituição de mensagens por strings para dicionários.

--- src/app/Node/Event.py
+++ src/app/Node/Event.py
@@ -1,5 +1,7 @@
 class Event:
-    def __init__(self, sender, timestamp, type_msg, id, msg='', number_of_acks=0, num_one_queue=0):
+    def __init__(self, sender, timestamp, type_msg, id, msg=None, number_of_acks=0, num_one_queue=0):
+        if msg is None:
+            msg = {}
         self.id: str = id
         self.sender = sender
         self.timestamp = timestamp
@@ -7,10 +9,14 @@ class Event:
         self.type_msg = type_msg
         self.number_of_acks: int = number_of_acks
         self.num_one_queue: int = num_one_queue
-        self.one_queue: bool = False
-        self.fail: bool = False
-        self.send: bool = False
-        self.loop_wait: bool = True
+
+        self.one_queue: bool = False  # é o primeiro da lista de todos os bancos
+        self.fail: bool = False  # Não é o primeiro da lista de algum banco
+        self.send: bool = False  # Controle de envio de mensagens para os peers
+        self.loop_wait: bool = True  # Controle do loop de envio de novas mensagens
+        self.exe: bool = False  # Já terminou de ser execultado pelo banco e sair do loop de espera de request
+        self.can_be_executed: bool = True  # Pode ser execultado pelo banco
+        self.mgs_executed: str = ""  # Mensagem que foi execultada ou nao
 
     def __lt__(self, other) -> bool:
         if self.timestamp == other.timestamp:
@@ -31,9 +37,10 @@ class Event:
             self.num_one_queue = 1
             if mgs == "ONE_QUEUE":
                 self.fail = False
+                self.can_be_executed = True
+                self.mgs_executed = ""
         else:
             self.num_one_queue += 1
 
     def set_one(self):
         self.one_queue = True
-
