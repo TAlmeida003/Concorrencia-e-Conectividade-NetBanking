730e4b1da6f6159c4560bed721306d64ccaadf96
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2024-06-24 22:50:39 -0300

    ✨ feat(utils): Adição da validação dos dados do usuário.

--- src/app/utils/utils.py
+++ src/app/utils/utils.py
@@ -1,5 +1,8 @@
 import random
+import re
 import time
+
+from src.app.Exception.UserException import UserException
 from src.app.Node import Event
 
 LIST_NODES: list[str] = ['3050', '3051', '3052']
@@ -54,3 +57,21 @@ def report_fail_node(id_node: int, dict_peers_online: dict[str:bool]) -> None:
     for node in LIST_NODES:
         if node != LIST_NODES[id_node]:
             dict_peers_online[node] = False
+
+
+def is_data_register_user_valid(user_name: str, name: str, num_cadastro: str, user_password: str, type_person: str):
+    cpf_standard = re.compile(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$')
+    cnpj_standard = re.compile(r'^\d{2}\.\d{3}\.\d{3}/\d{4}-\d{2}$')
+
+    if not bool(cpf_standard.match(num_cadastro)) and type_person == 'PF':
+        raise UserException('CPF inválido')
+    elif not bool(cnpj_standard.match(num_cadastro)) and type_person == 'PJ':
+        raise UserException('CNPJ inválido')
+    elif type_person != 'PF' and type_person != 'PJ':
+        raise UserException('Tipo de pessoa inválido')
+    elif len(user_password) < 8:
+        raise UserException('Senha deve ter no mínimo 8 caracteres')
+    elif len(name) < 3:
+        raise UserException('Nome inválido')
+    elif len(user_name) < 3:
+        raise UserException('Nome de usuário inválido')
