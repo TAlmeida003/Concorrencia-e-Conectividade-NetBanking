04bf1ac28525090dad7309c1c6e1496aaf4d59e6
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2024-06-29 19:10:39 -0300

    ✨ feat(enum): adição da opção pacote.

--- src/app/enums/__init__.py
+++ src/app/enums/__init__.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 class Option_Bank(Enum):
     REGISTER: str = "REGISTER"
+    PACKAGE: str = "PACKAGE"
     DEPOSIT: str = "DEPOSIT"
     WITHDRAW: str = "WITHDRAW"
     TRANSFER: str = "TRANSFER"
