a4c9cd5b32249754ad1941c00334331290cfa205
TAlmeida003 <108768985+TAlmeida003@users.noreply.github.com>
2024-06-29 19:13:28 -0300

    ✨ feat(API): Adição da endpoint para operações.

--- src/app/API/API.py
+++ src/app/API/API.py
@@ -1,5 +1,4 @@
 import os
-from typing import Tuple, List, Dict
 
 from src.app.Node.Node import Node
 from src.app.Node.Event import Event
@@ -10,20 +9,20 @@ from src.app.Exception.AccountException import AccountException
 from src.app.enums import Option_Bank
 from flask import Flask, jsonify, request, Response
 
-from threading import Thread
 
 app = Flask(__name__)
 
 PORT = os.getenv('P', '3050')
 node = Node(utils.LIST_NODES.index(PORT), utils.LIST_NODES)
 
-'''============= ROTAS DO NÓ =============='''
+'''
+                            ============= ROTAS DA ORDENAÇÃO TOTAL ==============
+'''
 
 
 @app.route('/proposer/<string:value>', methods=['POST'])
 def proposer(value):
     value = eval(value)
-    #Thread(target=node.propose_value, args=(value, "TESTE")).start()
     node.propose_value(value, "TESTE")
     return Response(status=200)
 
@@ -31,35 +30,27 @@ def proposer(value):
 @app.route('/receiver_message/', methods=['POST'])
 def receiver_message():
     event = request.json
-    event = Event(event['sender'], event['timestamp'], event['type_msg'], event['id'], event['msg'],
-                  event['number_of_acks'])
-
-    #Thread(target=node.receiver_message, args=(event,)).start()
+    event = Event(
+        event['sender'],
+        event['timestamp'],
+        event['type_msg'],
+        event['id'],
+        event['msg'],
+        event['number_of_acks']
+    )
     node.receiver_message(event)
     return Response(status=200)
 
 
 @app.route('/receiver_ack/<string:event_id>', methods=['POST'])
 def receiver_ack(event_id):
-    #Thread(target=node.receiver_ack, args=(event_id,)).start()
     node.receiver_ack(event_id)
     return Response(status=200)
-
-
-@app.route('/get_queue', methods=['GET'])
-def get_queue():
-    return jsonify([event.__dict__ for event in node.FIFO_evento])
-
-
-@app.route('/get_ack', methods=['GET'])
-def get_ack():
-    return jsonify(node.dict_ack)
 
 
 @app.route('/init_check_queue/<string:event_id>', methods=['POST'])
 def init_check_queue(event_id):
     dict_mgs = request.json
-    #Thread(target=node.send_one_queue, args=(event_id, dict_mgs)).start()
     node.send_one_queue(event_id, dict_mgs)
     return Response(status=200)
 
@@ -67,7 +58,6 @@ def init_check_queue(event_id):
 @app.route('/receiver_one_queue/<string:event_id>', methods=['POST'])
 def receiver_one_queue(event_id):
     mgs = request.json
-    #Thread(target=node.receiver_one_queue, args=(event_id, mgs)).start()
     node.receiver_one_queue(event_id, mgs)
     return Response(status=200)
 
@@ -83,7 +73,19 @@ def node_fail(node_id):
     return Response(status=200)
 
 
-'''============= ROTAS  DO BANCO =============='''
+@app.route('/get_queue', methods=['GET'])
+def get_queue():
+    return jsonify([event.__dict__ for event in node.FIFO_evento])
+
+
+@app.route('/get_ack', methods=['GET'])
+def get_ack():
+    return jsonify(node.dict_ack)
+
+
+'''
+                                    ============= ROTAS  DO BANCO ==============                                 
+'''
 
 
 @app.route('/register-customer', methods=['POST'])
@@ -91,9 +93,13 @@ def register_customer() -> tuple[Response, int]:
     dict_customer: dict = request.json
 
     try:
-        utils.is_data_register_user_valid(dict_customer['name'], dict_customer['user_name'],
-                                          dict_customer['num_cadastro'], dict_customer['password'],
-                                          dict_customer['type_person'])
+        utils.is_data_register_user_valid(
+            dict_customer['name'],
+            dict_customer['user_name'],
+            dict_customer['num_cadastro'],
+            dict_customer['password'],
+            dict_customer['type_person']
+        )
 
         event: Event = node.propose_value(dict_customer, Option_Bank.REGISTER.value)
 
@@ -113,7 +119,10 @@ def register_customer() -> tuple[Response, int]:
 def login() -> tuple[Response, int]:
     dict_login: dict = request.json
     try:
-        dict_data_user: dict[str, str | dict] = node.bank.login(dict_login['user_name'], dict_login['password'])
+        dict_data_user: dict[str, str | dict] = node.bank.login(
+            dict_login['user_name'],
+            dict_login['password']
+        )
         return jsonify({"descript": "Logado com sucesso", "data": dict_data_user}), 200
     except (BankException, UserException) as e:
         return jsonify({'descript': e.__str__()}), 400
@@ -124,8 +133,14 @@ def create_account() -> tuple[Response, int]:
     dict_account: dict = request.json
 
     try:
-        node.bank.create_conta(dict_account['user_name'], dict_account['type_account'], dict_account['password'],
-                               dict_account['pix_type'], dict_account['users'])
+        node.bank.create_conta(
+            dict_account['user_name'],
+            dict_account['type_account'],
+            dict_account['password'],
+            dict_account['pix_type'],
+            dict_account['users'],
+            dict_account['value_init']
+        )
         return jsonify({'descript': 'Conta criada com sucesso'}), 200
     except (BankException, AccountException) as e:
         return jsonify({'descript': e.__str__()}), 400
@@ -135,10 +150,43 @@ def create_account() -> tuple[Response, int]:
 def get_count(user_name: str) -> tuple[dict[int, list[list[int | float]]], int]:
     list_accounts_value: list[list[int | float]] = []
     for account in node.bank.dict_user[user_name].accounts:
-        list_accounts_value.append([account, node.bank.dict_account[account].balance])
-    return {utils.LIST_NODES.index(PORT): list_accounts_value}, 200
+        list_accounts_value.append([account, node.bank.dict_account[account].balance, node.bank.dict_account[account].kay_pix])
+    return {str(utils.LIST_NODES.index(PORT)): list_accounts_value}, 200
 
 
 @app.route('/get_users', methods=['GET'])
 def get_users() -> tuple[Response, int]:
     return jsonify([user.__dict__ for user in node.bank.dict_user.values()]), 200
+
+
+@app.route('/operations', methods=['POST'])
+def operations() -> tuple[Response, int]:
+    operations_package: dict = request.json
+
+    event: Event = node.propose_value(operations_package, Option_Bank.PACKAGE.value)
+
+    while not event.exe:
+        pass
+
+    if event.can_be_executed:
+        return jsonify({"descript": "Operações realizada com sucesso"}), 200
+    else:
+        return jsonify({'descript': event.mgs_executed}), 400
+
+
+@app.route('/receiver_pix', methods=['POST'])
+def receiver_pix() -> Response:
+    dict_data: dict = request.json
+    node.bank.receiver_pix(dict_data)
+    return Response(status=200)
+
+
+@app.route('/receiver_pix_all/<string:pix>', methods=['POST'])
+def receiver_pix_all(pix: str) -> Response:
+    node.bank.list_pix_all.append(pix)
+    return Response(status=200)
+
+
+@app.route('/get_pix_all', methods=['GET'])
+def get_pix_all() -> tuple[Response, int]:
+    return jsonify(node.bank.list_pix_all), 200
